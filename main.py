9ec6582efcbd274c283c84672acfd54ecf3a3467
Seongyeol Yun <02tjdduf@gmail.com>
2022-10-13 23:44:59 +0900

    main.py 추가

--- main.py
+++ main.py
@@ -1,45 +1,9 @@
 from pico2d import *
+import game_framework
+import play_state
+import title_state
 
-class World:
-    def __init__(self):
-        self.image = load_image("Resources/Map/World1-1.png")
+pico2d.open_canvas()
+game_framework.run(title_state)
+pico2d.close_canvas()
 
-    def render(self):
-       self.image.draw(3280//2,232//2)
-
-class BackGround:
-    def __init__(self):
-        self.image = load_image("Resources/Map/back_ground.png")
-
-    def render(self):
-        self.image.draw(0,600,3280,1200)
-
-def handle_events():
-    global running
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            ruuning = False
-
-open_canvas()
-
-back_ground = BackGround()
-world = World()
-running = True
-
-# Game main loop  
-while running:
-    #Input Process 
-    handle_events()
-
-    #Game logic - update
-
-    #Rendering
-    back_ground.render()
-    world.render()
-    delay(0.05)
-    update_canvas()
-
-close_canvas()
