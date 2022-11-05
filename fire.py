cb234c2a459a0af9284e0541ab4dd389f0422c13
Seongyeol Yun <02tjdduf@gmail.com>
2022-11-05 23:25:24 +0900

    fire 모션 추가

---
+++ fire.py
@@ -0,0 +1,22 @@
+from pico2d import *
+
+import game_world
+
+class Fire:
+    image = None
+
+    def __init__(self, x = 800, y = 300, velocity = 1):
+        if Fire.image == None:
+            Fire.image = load_image("Resources/Mario/fire.png")
+        
+        self.x, self.y, self.velocity = x, y,velocity
+    
+    def draw(self):
+        self.image.draw(self.x,self.y)
+
+    def update(self):
+        self.x += self.velocity
+
+        if self.x < 20 or self.x > 800 -20:
+            game_world.remove_object(self)
+
