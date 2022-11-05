8774385ce7749e415000fd06082d1651a930ac45
Seongyeol Yun <02tjdduf@gmail.com>
2022-11-05 23:41:43 +0900

    change framework

--- play_state.py
+++ play_state.py
@@ -1,17 +1,15 @@
 from pico2d import *
 import game_framework
+import game_world
 import title_state
+
 from mario import Mario
 from world import World
-from background import BackGround
 
-back_ground = None
 world = None
 mario = None
 
 def handle_events():
-    global mario
-    global world 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -20,79 +18,43 @@ def handle_events():
             game_framework.quit()
         else:
             mario.handle_event(event)
-    # for event in events:
-    #     if event.type == SDL_QUIT:
-    #         game_framework.quit()
-    #     elif event.type == SDL_KEYDOWN:
-    #         if event.key == SDLK_LEFT:
-    #             mario.dir -= 1
-    #             mario.frame_bottom = 80
-    #             #world.offset_x += 50
-    #             pass
-    #         elif event.key == SDLK_RIGHT:
-    #             mario.dir += 1
-    #             mario.frame_bottom = 120
-    #             #world.offset_x -= 50
-    #         elif event.key == SDLK_SPACE:
-    #             if mario.is_jump == 0:
-    #                 mario.jump(1)
-    #             elif mario.is_jump ==1:
-    #                 mario.jump(2)
-    #         elif event.key == SDLK_ESCAPE:
-    #             game_framework.change_state(title_state)
-    #     elif event.type == SDL_KEYUP:
-    #         if event.key == SDLK_LEFT:
-    #             mario.dir += 1
-    #             mario.frame_bottom = 160
-    #             pass
-    #         elif event.key == SDLK_RIGHT:
-    #             mario.dir -= 1
-    #             mario.frame_bottom = 200
-    #             pass
-
 
 def enter():
-    global back_ground, world, mario
-    back_ground = BackGround()
+    global world, mario
     world = World()
     mario = Mario()
+    game_world.add_object(world,0)
+    game_world.add_object(mario,1)
+
+def exit():
+    game_world.clear()
 
 def update():
-    global mario
-    mario.update()
-    pass
-
-
-def draw_back_ground():
-    back_ground.draw()
-
+    for game_object in game_world.all_objects():
+        game_object.update()
 
 def draw_wolrd():
-    world.draw()
-
-
-def draw_mario():
-    mario.draw()
-
+    for game_object in game_world.all_objects():
+        game_object.draw()
 
 def draw():
     clear_canvas()
-    #draw_back_ground()
     draw_wolrd()
-    draw_mario()
     delay(0.05)
     update_canvas()
 
 def pause():
     pass
 
-
 def resume():
     pass
 
+def test_self():
+    import play_state
 
-def exit():
-    global back_ground, world, mario
-    del back_ground
-    del world
-    del mario
+    pico2d.open_canvas()
+    game_framework.run(play_state)
+    pico2d.clear_canvas()
+
+if __name__ == '__main__':
+    test_self()
