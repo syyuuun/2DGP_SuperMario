97499c5253610e610267719f3540e20a2e427cf5
Seongyeol Yun <02tjdduf@gmail.com>
2022-11-11 01:13:21 +0900

    add frame time

--- game_framework.py
+++ game_framework.py
@@ -8,8 +8,6 @@ class GameState:
         self.update = state.update
         self.draw = state.draw
 
-
-
 class TestGameState:
 
     def __init__(self, name):
@@ -94,6 +92,9 @@ def fill_states(*states):
     for state in states:
         stack.append(state)
 
+import time
+frame_time = 0
+
 def run(start_state):
     global running, stack
     running = True
@@ -105,10 +106,18 @@ def run(start_state):
 
     stack.append(start_state)
     stack[-1].enter()
+
+    current_time = time.time()
+
     while running:
         stack[-1].handle_events()
         stack[-1].update()
         stack[-1].draw()
+        global frame_time
+        frame_time = time.time() - current_time
+        frame_rate = 1 / frame_time
+        current_time += frame_time
+
     # repeatedly delete the top of the stack
     while (len(stack) > 0):
         stack[-1].exit()
@@ -118,8 +127,6 @@ def run(start_state):
 def test_game_framework():
     start_state = TestGameState('StartState')
     run(start_state)
-
-
 
 if __name__ == '__main__':
     test_game_framework()
