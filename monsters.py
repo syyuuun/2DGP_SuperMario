8e5e2255a5569d607da41929446ce3faaf6dacb9
Seongyeol Yun <02tjdduf@gmail.com>
2022-11-12 02:24:11 +0900

    add monsters die animation

--- monsters.py
+++ monsters.py
@@ -13,11 +13,16 @@ class Goomba:
         self.frame_bottom = 0
         self.temp_x = 0
         self.move_amount = MOVE_AMOUNT
+        self.sprite_width = 47
+        self.sprite_height = 61
+        self.is_collision = False
+        self.timer = 5
 
     def update(self):
-        self.x += self.face_dir * self.move_amount
         self.frame = (self.frame+1) % 8
-        self.temp_x += MOVE_AMOUNT
+        if self.is_collision == False:
+            self.x += self.face_dir * self.move_amount
+            self.temp_x += MOVE_AMOUNT
 
         if self.temp_x > 100:
             self.temp_x  =0
@@ -26,16 +31,28 @@ class Goomba:
             elif self.face_dir ==1:
                 self.face_dir =-1
 
+        if self.is_collision == True:
+            self.timer -=1
+            print(self.timer)
+            if self.timer < 0:
+                game_world.remove_object(self)
+
     def draw(self):
-        if self.face_dir == -1:
-            self.frame_bottom = 62
-            pass
-        elif self.face_dir == 1:
-            self.frame_bottom = 0
-            pass
+        if self.is_collision == True:
+            self.frame_bottom = 164
+            self.sprite_height = 54
+            self.sprite_width = 63
+            self.frame_bottom = 126
+        else:
+            if self.face_dir == -1:
+                self.frame_bottom = 62
+                pass
+            elif self.face_dir == 1:
+                self.frame_bottom = 0
+                pass
         
         self.image.clip_draw(
-             self.frame * 47, self.frame_bottom, 47, 61, self.x, self.y
+             self.frame * self.sprite_width, self.frame_bottom, self.sprite_width, self.sprite_height, self.x, self.y
         )
         
         draw_rectangle(*self.get_bb())
@@ -49,7 +66,7 @@ class Goomba:
     def handle_collision(self,other,group):
         print("goomba die")        
         if group == "mario:goombas":
-            game_world.remove_object(self)
+            self.is_collision = True
 
 class Troopa:
     def __init__(self):
@@ -60,11 +77,16 @@ class Troopa:
         self.frame_bottom = 0
         self.temp_x = 0
         self.move_amount = MOVE_AMOUNT
+        self.sprite_width  = 53
+        self.sprite_height = 61
+        self.is_collision = False
+        self.timer = 5
 
     def update(self):
-        self.x += self.face_dir * self.move_amount
         self.frame = (self.frame+1) % 8
-        self.temp_x += MOVE_AMOUNT
+        if self.is_collision == False:
+            self.x += self.face_dir * self.move_amount
+            self.temp_x += MOVE_AMOUNT
 
         if self.temp_x > 100:
             self.temp_x  =0
@@ -73,16 +95,26 @@ class Troopa:
             elif self.face_dir ==1:
                 self.face_dir =-1
 
+        if self.is_collision == True:
+            self.timer -=1
+            print(self.timer)
+            if self.timer < 0:
+                game_world.remove_object(self)
+
     def draw(self):
-        if self.face_dir == -1:
-            self.frame_bottom = 0
-            pass
-        elif self.face_dir == 1:
-            self.frame_bottom = 61
-            pass
+        if self.is_collision == True:
+            self.sprite_width = 32
+            self.sprite_height = 30
+            self.frame_bottom = 126
+            self.y = 48
+        else:
+            if self.face_dir == -1:
+                self.frame_bottom = 0
+            elif self.face_dir == 1:
+                self.frame_bottom = 61
         
         self.image.clip_draw(
-             self.frame * 53, self.frame_bottom, 53, 61, self.x, self.y
+             self.frame * self.sprite_width, self.frame_bottom, self.sprite_width, self.sprite_height, self.x, self.y
         )
         
         draw_rectangle(*self.get_bb())
@@ -97,4 +129,4 @@ class Troopa:
         print("troopa die")
     
         if group == "mario:troopas":
-            game_world.remove_object(self)
+            self.is_collision = True
