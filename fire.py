dc7a2d2b04b26fdabdf4e255782b81aeb65d400b
Seongyeol Yun <02tjdduf@gmail.com>
2022-11-27 20:51:30 +0900

    add fire:koopa

--- fire.py
+++ fire.py
@@ -4,18 +4,28 @@ import game_world
 
 class Fire:
     image = None
-
     def __init__(self, x = 800, y = 300, velocity = 1):
         if Fire.image == None:
-            Fire.image = load_image("Resources/Mario/fire.png")
-        
+            Fire.image = load_image("Resources/Mario/fire.png")        
+        # fire position    
         self.x, self.y, self.velocity = x, y,velocity
+        self.is_hit = False
     
     def draw(self):
         self.image.draw(self.x,self.y)
+        draw_rectangle(*self.get_bb())
 
     def update(self):
-        self.x += self.velocity
-
-        if self.x < 20 or self.x > 800 -20:
+        # fire position 
+        if self.is_hit == True:
             game_world.remove_object(self)
+        else:
+            self.x += self.velocity
+
+    def get_bb(self):
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
+    def handle_collision(self,other,group):
+        if group == "fire:goombas" or group=="fire:troopas" or group == "fire:koopa":
+            self.is_hit = True
+        
