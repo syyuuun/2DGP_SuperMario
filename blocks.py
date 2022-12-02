5297b41398f98dd4c21349f9f3bde25526183ce2
Seongyeol Yun <02tjdduf@gmail.com>
2022-12-02 18:11:45 +0900

    아이템박스 충돌처리 구현

--- blocks.py
+++ blocks.py
@@ -1,10 +1,19 @@
 from pico2d import *
 from random import randint
 import game_world
+from items import FireFlower
+from items import Mushroom
+from items import LifeUpMushroom
+from items import Coin
 #import play_state
 import server
 
-class ItemBlocks:
+flower = None
+coin = None
+mushroom = None
+life_mushroom = None
+
+class ItemBlocks: 
     def __init__(self, x = 0, y = 0):
         self.image = load_image("Resources/Blocks/item_block.png")
         self.canvas_width = get_canvas_width()
@@ -27,7 +36,33 @@ class ItemBlocks:
         return self.sx - 8, self.sy - 8, self.sx + 8, self.sy + 8
 
     def handle_collision(self,other,group):
-        pass
+        print("collision")
+        global flower, mushroom,life_mushroom, coin
+        x,y = self.x, self.y +10
+      
+        if "mario:item_blocks" == group:
+            random = randint(0,3)
+            if 0 == random:
+                flower = FireFlower(x,y)
+                game_world.add_object(flower,1)
+                game_world.add_collision_group(server.mario,flower,"mario:fire_flowers")
+                game_world.remove_object(self)
+            elif 1 == random:
+                coin = Coin(x,y)
+                game_world.add_object(coin,1)
+                game_world.add_collision_group(server.mario,coin,"mario:coins")
+                game_world.remove_object(self)
+            elif 2 == random:
+                mushroom = Mushroom(x,y)
+                game_world.add_object(mushroom,1)
+                game_world.add_collision_group(server.mario,mushroom,"mario:mushrooms")
+                game_world.remove_object(self)
+            
+            elif 3 == random:
+                life_mushroom  = LifeUpMushroom(x,y)
+                game_world.add_object(life_mushroom,1)
+                game_world.add_collision_group(server.mario,life_mushroom,"mario:life_up_mushrooms")
+                game_world.remove_object(self)
 
 class FloorBrick:
     def __init__(self, x = 0, y = 0):
--- items.py
+++ items.py
@@ -10,7 +10,6 @@ class Mushroom:
         self.x, self.y =x, y
         self.is_collision = False
         self.sx,self.sy = 0, 0
-    
     def update(self):
         pass
     
@@ -25,6 +24,7 @@ class Mushroom:
     
     def handle_collision(self,other,group):
         if group == "mario:mushrooms":
+            self.is_collision = True
             game_world.remove_object(self)
 
 class FireFlower:
@@ -70,5 +70,31 @@ class LifeUpMushroom:
         return self.sx-8,self.sy-8,self.sx+8,self.sy+8
     
     def handle_collision(self,other,group):
-        if group == "mario:stars":
+        if group == "mario:life_up_mushrooms":
             game_world.remove_object(self)
+
+class Coin:
+    def __init__(self, x = 0, y = 0):
+        self.image = load_image("Resources/Items/coin.png")
+        self.frame = 0
+        self.x, self.y = 400, 50
+        self.sprite_width, self.sprite_height = 16,16
+        self.is_collision = False
+        self.sx, self.sy = 0, 0
+
+    def update(self):
+        self.frame = (self.frame+1) % 4
+        
+    def draw(self):
+        self.sx, self.sy = self.x - server.background.window_left, self.y - server.background.window_bottom
+        self.image.clip_draw(self.frame * self.sprite_width, 0, self.sprite_width, self.sprite_height, self.sx, self.sy)
+        draw_rectangle(*self.get_bb())
+        
+
+    def get_bb(self):
+        return self.sx-8,self.sy-8,self.sx+8,self.sy+8
+    
+    def handle_collision(self,other,group):
+        if group == "mario:coins":
+            game_world.remove_object(self)
+    pass 
